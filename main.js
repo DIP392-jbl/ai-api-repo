0f81f6403eec8a3a3ecb666fee38783c4ed0c0e6
klek6 <kleksisjava@gmail.com>
2024-02-27 23:28:23 +0200

    nozīmīgs komentārs par izmaiņān

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
-
+// omg izmaiņas
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
